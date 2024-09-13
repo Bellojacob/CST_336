7553b0cad6020e5d20476ca71dfc51af3e3cc6eb
Jacob Bello <jacobbello2002@gmail.com>
2024-09-12 21:02:52 -0700

    lab2

---
+++ labs/lab2/js/script.js
@@ -0,0 +1,57 @@
+//Event listener
+document.querySelector("#myButton").addEventListener("click", checkNum);
+
+let currentGuessCount = 1;
+let playerWins = 0;
+let playerLosses = 0;
+
+function checkNum(){
+    console.log("Checking the number");
+    document.querySelector("#resetButton").style.display = "none";
+    let userGuess = document.querySelector("#input").value;
+    //alert("You entered: " + userGuess);
+
+    document.querySelector("#previousGuesses").innerText += " " + userGuess;
+    document.querySelector("#winslosses").innerText = "Wins: " + playerWins + " Losses: " + playerLosses;
+
+    if(currentGuessCount <= 7){ 
+        console.log(currentGuessCount);
+    if(userGuess == randomNum){
+        document.querySelector("#feedback").innerText = "Correct";
+        document.querySelector("#feedback").style.color = "green";
+        playerWins++;
+        return;
+    } else if(userGuess > randomNum){
+        document.querySelector("#feedback").innerText = "Too High";
+        document.querySelector("#feedback").style.color = "red";
+        currentGuessCount++;
+    } else if(userGuess < randomNum){
+        document.querySelector("#feedback").innerText = "Too Low";
+        document.querySelector("#feedback").style.color = "red";
+        currentGuessCount++;
+    } else {
+        document.querySelector("#feedback").innerText = "Invalid input";
+        document.querySelector("#feedback").style.color = "red";
+    }
+} else {
+    document.querySelector("#feedback").innerText = "Game Over";
+    document.querySelector("#correctAnswer").innerText = "Correct Answer: " + randomNum;
+    document.querySelector("#feedback").style.color = "black";
+    playerLosses++;
+    
+    document.querySelector("#resetButton").style.display = "block";
+    currentGuessCount = 0;  
+    userGuess = "";
+    document.querySelector("#previousGuesses").innerText = " " + userGuess;
+
+    
+}
+}
+
+
+
+// random number
+let randomNum = (Math.random() * 99) + 1;
+randomNum = Math.floor(randomNum);
+console.log(randomNum);
+
