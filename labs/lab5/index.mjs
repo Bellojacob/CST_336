cac84e024494034ebaf1b8443db59f0ec6cee136
Jacob Bello <jacobbello2002@gmail.com>
2024-10-10 21:22:36 -0700

    lab5

---
+++ labs/lab5/index.mjs
@@ -0,0 +1,36 @@
+import express from 'express';
+const planets = (await import ('npm-solarsystem')).default;
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+   res.render("home.ejs")
+});
+
+app.get('/mercury', (req, res) => {
+    let mercury = planets.getMercury();
+    res.render("mercury.ejs", {mercury})
+ });
+
+ app.get('/venus', (req, res) => {
+    let venus = planets.getVenus();
+    res.render("venus.ejs", {venus})
+ });
+
+ app.get('/mars', (req, res) => {
+    let mars = planets.getMars();
+    res.render("mars.ejs", {mars})
+ });
+
+
+
+ app.get('/saturn', (req, res) => {
+    let saturn = planets.getSaturn();
+    res.render("saturn.ejs", {saturn})
+ });
+
+app.listen(3000, () => {
+   console.log('server started');
+});
