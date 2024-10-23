93cf3497305d802b9bd7e5bd2c6e7ef2602c6ec9
Jacob Bello <jacobbello2002@gmail.com>
2024-10-22 21:28:33 -0700

    in class

---
+++ practice/db-intro/index.mjs
@@ -0,0 +1,38 @@
+import express from 'express';
+import mysql from 'mysql2/promise';
+
+const app = express();
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+const pool = mysql.createPool({
+    host: "jacob-bello.tech",
+    user: "jacobbel_webuser",
+    password: "cst-336",
+    database: "jacobbel_quotes",
+    connectionLimit: 10,
+    waitForConnections: true
+});
+const conn = await pool.getConnection();
+
+//routes
+app.get('/', (req, res) => {
+   res.send('Hello Express app!')
+});
+
+app.get("/allAuthors", async(req, res) => {
+    let sql = "SELECT * FROM authors";
+    const [rows] = await conn.query(sql);
+    res.send(rows);
+});//dbTest
+
+app.get("/dbTest", async(req, res) => {
+    let sql = "SELECT CURDATE()";
+    const [rows] = await conn.query(sql);
+    res.send(rows);
+});//dbTest
+
+app.listen(3001, ()=>{
+    console.log("Express server running")
+})
