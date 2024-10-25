ccc592e6546d08fdcf7f7b13541d7fb36d3578ef
Bellojacob <jacobbello2002@gmail.com>
2024-10-25 11:28:51 -0700

    push lab5

--- labs/lab5-practice/index.mjs
+++ labs/lab5-practice/index.mjs
@@ -1,12 +1,33 @@
 import express from 'express';
 const planets = (await import ('npm-solarsystem')).default;
+import fetch from 'node-fetch';
+
+
+// To whoever grades this, I know it isn't a good idea to keep my key here however, I don't know of another solution currently, and
+// it is a free api, so I mean, please don't steal it but I 99% would never know and it wouldn't effect me, 
+// and I am running out of time to complete this. 
+let myKey = "qkjYSPgFa3FJabNaNBcFWhNaaJEHAoElbeiVXY4C"
+
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-   res.render("home.ejs")
+app.get('/', async (req, res) => {
+   let imgUrl;
+   let nasaUrl = `https://api.nasa.gov/planetary/apod?api_key=${myKey}&count=1`;
+
+   try {
+   
+       const response = await fetch(nasaUrl);
+       const data = await response.json();
+       imgUrl = data[0].url; 
+   } catch (error) {
+       console.error("Error fetching NASA APOD image:", error);
+       imgUrl = "/path/to/default-image.jpg";
+   }
+
+   res.render("home.ejs", { imgUrl });
 });
 
 
@@ -73,14 +94,39 @@ app.get('/neptune', (req, res) => {
    res.render("neptune.ejs", {neptune})
 });
 
-app.get('/nasa', (req, res) => {
-   
-   res.render("nasa.ejs")
+app.get('/nasa', async (req, res) => {
+   let { yearSelected, monthSelected, daySelected } = req.query;
+
+
+   if (!yearSelected || !monthSelected || !daySelected) {
+       yearSelected = new Date().getFullYear();
+       monthSelected = ('0' + (new Date().getMonth() + 1)).slice(-2);
+       daySelected = ('0' + new Date().getDate()).slice(-2);
+   }
+
+   let date = `${yearSelected}-${monthSelected}-${daySelected}`;
+   console.log("Selected date: " + date);
+
+   let url = `https://api.nasa.gov/planetary/apod?api_key=${myKey}&date=${date}`;
+   try {
+       let response = await fetch(url);
+       let data = await response.json();
+       
+       let imgUrl = data.url;
+       res.render("nasa.ejs", { imgUrl });
+   } catch (error) {
+       console.error(error);
+       res.status(500).send("Error fetching data from NASA API");
+   }
 });
 
+// let url = `https://csumb.space/api/cityInfoAPI.php?zip=${zipCode}`;
+// let response = await fetch(url);
+// let data = await response.json();
+// console.log(data);
 
 
-
- app.listen(3000, () => {
+ app.listen(10009, () => {
    console.log('server started');
 });
+
