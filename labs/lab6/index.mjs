d787a4f06466373d4324ebdc6b3cbb282482a67e
Jacob Bello <jacobbello2002@gmail.com>
2024-10-30 12:18:55 -0700

    lab6 push

---
+++ labs/lab6/index.mjs
@@ -0,0 +1,47 @@
+import express from 'express';
+import mysql from 'mysql2/promise';
+
+const app = express();
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+const pool = mysql.createPool({
+    host: "jacob-bello.tech",
+    user: "jacobbel_webuser",
+    password: "cst-336",
+    database: "jacobbel_quotes",
+    connectionLimit: 10,
+    waitForConnections: true
+});
+const conn = await pool.getConnection();
+
+//routes
+app.get('/', (req, res) => {
+   res.render('home.ejs')
+});
+
+app.get('/searchByKeyword', async (req, res) => {
+    let keyword = req.query.keyword;
+    let sql = `SELECT firstName, lastName, quote 
+               FROM quotes
+               NATURAL JOIN authors 
+               WHERE quote LIKE ?`;
+    let sqlParams = [`%${keyword}%`];
+    const [rows] = await conn.query(sql, sqlParams);
+    console.log(keyword);
+    res.render("quotes.ejs", {rows})
+ });
+ 
+
+
+
+app.get("/dbTest", async(req, res) => {
+    let sql = "SELECT CURDATE()";
+    const [rows] = await conn.query(sql);
+    res.send(rows);
+});//dbTest
+
+app.listen(5001, ()=>{
+    console.log("Express server running")
+})
